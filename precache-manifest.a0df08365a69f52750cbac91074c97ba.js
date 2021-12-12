7f1906771a034f69cfc773ffcecd311240522f3e
ninanolets <marinaa.noleto@gmail.com>
2021-12-12 19:48:32 +0100

    Updates

---
+++ precache-manifest.a0df08365a69f52750cbac91074c97ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da73c081c529ee51beb528e17a03d053",
+    "url": "/retink_app/index.html"
+  },
+  {
+    "revision": "33634e116fd5882dbd52",
+    "url": "/retink_app/static/css/main.1f17ec8b.chunk.css"
+  },
+  {
+    "revision": "a0d68ea5169bef520b0e",
+    "url": "/retink_app/static/js/2.0c81f069.chunk.js"
+  },
+  {
+    "revision": "14fcb7e3071ce566acde5dc78bd60fd5",
+    "url": "/retink_app/static/js/2.0c81f069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33634e116fd5882dbd52",
+    "url": "/retink_app/static/js/main.34b594c2.chunk.js"
+  },
+  {
+    "revision": "6bbe58ccaf678202fb99",
+    "url": "/retink_app/static/js/runtime-main.7d87f2ec.js"
+  },
+  {
+    "revision": "181d28301e347ffb1c16fecae82805a3",
+    "url": "/retink_app/static/media/IconHat.181d2830.svg"
+  },
+  {
+    "revision": "2a464042b94fd37e60a686f58840d27a",
+    "url": "/retink_app/static/media/White.2a464042.svg"
+  },
+  {
+    "revision": "8632637371bbee4e35d6940196a7d686",
+    "url": "/retink_app/static/media/bg.86326373.svg"
+  },
+  {
+    "revision": "e3e721db4d64a35b494fc8a4311e0f0f",
+    "url": "/retink_app/static/media/mascot.e3e721db.png"
+  }
+]);
